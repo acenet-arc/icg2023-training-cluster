223e1f0fc49907be1d17e98aa8836784ff094915
Chris Geroux <chris.m.geroux@gmail.com>
2023-04-21 15:28:54 -0300

    created a small and large node

--- main.tf
+++ main.tf
@@ -19,7 +19,8 @@ module "openstack" {
   instances = {
     mgmt   = { type = "p4-6gb", tags = ["puppet", "mgmt", "nfs"], count = 1 }
     login  = { type = "p2-3gb", tags = ["login", "public", "proxy"], count = 1 }
-    node   = { type = "p2-3gb", tags = ["node"], count = 1 }
+    smallnode   = { type = "p4-6gb", tags = ["node"], count = 1 }
+    largenode   = { type = "p8-12gb", tags = ["node"], count = 1 }
   }
 
   # var.pool is managed by Slurm through Terraform REST API.
